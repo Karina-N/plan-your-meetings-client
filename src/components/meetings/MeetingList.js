d523015c48ef9b6b4c6f31b5a5cd7d547b2f2459
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-09-29 10:33:27 +0300

    remove calendar icon-it overlaps with text when weekday is 'longer'

--- src/components/meetings/MeetingList.js
+++ src/components/meetings/MeetingList.js
@@ -67,21 +67,23 @@ class MeetingList extends React.Component {
     );
   }
 
-  renderCalendarIcon() {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        class="bi bi-calendar-event"
-        viewBox="0 0 16 16"
-      >
-        <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
-        <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-      </svg>
-    );
-  }
+  // CALENDAR ICON
+
+  // renderCalendarIcon() {
+  //   return (
+  //     <svg
+  //       xmlns="http://www.w3.org/2000/svg"
+  //       width="16"
+  //       height="16"
+  //       fill="currentColor"
+  //       class="bi bi-calendar-event"
+  //       viewBox="0 0 16 16"
+  //     >
+  //       <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
+  //       <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+  //     </svg>
+  //   );
+  // }
 
   renderLocationIcon() {
     return (
@@ -117,10 +119,7 @@ class MeetingList extends React.Component {
 
     return (
       <>
-        <h6 className="today-date">
-          <span>{this.renderCalendarIcon()}</span>
-          {todayDate}
-        </h6>
+        <h6 className="today-date">{todayDate}</h6>
         <div>
           {this.props.listOfClients.length ? (
             this.props.meetingsList.length ? (
